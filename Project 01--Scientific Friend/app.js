3dd584edda67e8fc723ed2d1aabadfc7c8ae3414
Lu Zhang <lz3281@nyu.edu>
2024-09-24 17:00:00 +0800

    Add files via upload
    project01 scientific friend

---
+++ Project 01--Scientific Friend/app.js
@@ -0,0 +1,45 @@
+window.addEventListener("load", function () {
+    this.fetch('data.json')
+        .then(response => response.json())
+
+        .then(data => {
+            let friendArray = data.friend;
+            let randomNumber = Math.floor(Math.random() * friendArray.length);
+            let nameElement = document.getElementById("friend-name");
+            nameElement.innerHTML = data.friend[randomNumber].Name;
+        })
+
+    let button = document.getElementById("friend-button");
+    button.addEventListener("click", function () {
+        let inputText = document.getElementById("friend-input").value.toLowerCase();
+
+
+        let API_URL = "data.json" ;
+        fetch(API_URL)
+            .then(response => response.json())
+            .then(data => {
+                let friendArray = data.friend;
+                let foundFriend = friendArray.find(friend => friend.Name.toLowerCase() === inputText);
+
+                if (foundFriend) {
+                    let resultElement = document.getElementById("friend-result");
+                    resultElement.innerHTML = `
+                <p>Name: ${foundFriend.Name}</p>
+                <p>Age: ${foundFriend.Age}</p>
+                <p>Pronouns: ${foundFriend.Pronouns}</p>
+                <p>From: ${foundFriend.From}</p>
+                <p>Friendship Motto: ${foundFriend.FriendshipMotto}</p>
+                <p>Friendship Preferences: ${foundFriend.FriendshipPreferences}</p>
+            `;
+
+                } else {
+                    console.log("No matching friend found.");
+                    let resultElement = document.getElementById("friend-result");
+                    resultElement.innerHTML = `<p>No matching friend found for "${inputText}".</p>`;
+                }
+            })
+
+    })
+
+})
+
